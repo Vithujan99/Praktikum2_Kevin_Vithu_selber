43e769413c5475b8c2e3eb5fa78937d377a95cce
Vithujan99 <vithujan99@hotmail.com>
2022-11-19 15:05:56 +0100

    private Praktikum vorberitung

---
+++ src/test/java/zinsrechner/ZinsrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package zinsrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZinsrechnerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
